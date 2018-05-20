9da5d7a6a557e719864ddb2b2144ffd6fde921d9
Santiago Varela <santiagovarela.86@gmail.com>
2018-05-20 02:04:56 -0300

    Se actualiza DER. Se actualiza Script Creacion Inicial

--- ScriptCreacionInicial/script_creacion_inicial.sql
+++ ScriptCreacionInicial/script_creacion_inicial.sql
@@ -1,23 +1,18 @@
-DROP TABLE LOS_BORBOTONES.Rol
-DROP TABLE LOS_BORBOTONES.Funcionalidad
-DROP TABLE LOS_BORBOTONES.Funcionalidad_X_Rol
-DROP SCHEMA LOS_BORBOTONES
-
-CREATE SCHEMA [LOS_BORBOTONES] AUTHORIZATION [dbo]
+CREATE SCHEMA LOS_BORBOTONES AUTHORIZATION gdHotel2018
 
 CREATE TABLE LOS_BORBOTONES.Rol (
 
-	idRol	INT			NOT NULL,
-	Nombre	VARCHAR(45) NOT NULL,
-	Estado	VARCHAR(45)	NOT NULL,
+	idRol	INT			IDENTITY(1,1)	NOT NULL,
+	Nombre	VARCHAR(45) 				NOT NULL,
+	Activo	BIT							NOT NULL,
 	PRIMARY KEY(idRol)
 
 )
 
 CREATE TABLE LOS_BORBOTONES.Funcionalidad (
 
-	idFuncionalidad	INT			NOT NULL,
-	Descripcion 	VARCHAR(45)	NOT NULL,
+	idFuncionalidad	INT			IDENTITY(1,1)	NOT NULL,
+	Descripcion 	VARCHAR(45)					NOT NULL,
 	PRIMARY KEY (idFuncionalidad)
 
 )
@@ -26,6 +21,17 @@ CREATE TABLE LOS_BORBOTONES.Funcionalidad_X_Rol (
 
 	idFuncionalidad	INT			NOT NULL,
 	idRol			INT			NOT NULL,
-	PRIMARY KEY (idFuncionalidad, idRol)
-
+	PRIMARY KEY (idFuncionalidad, idRol),
+	FOREIGN KEY (idFuncionalidad) REFERENCES Funcionalidad (idFuncionalidad),
+	FOREIGN KEY (idRol) REFERENCES Rol (idRol),
 )
+
+GO
+
+INSERT INTO LOS_BORBOTONES.Rol (Nombre, Activo)
+VALUES ('Administrador', 1), ('Recepcionista', 1), ('Guest', 1);
+
+INSERT INTO LOS_BORBOTONES.Funcionalidad (Descripcion)
+VALUES ('ABMRoles'), ('ABMReservas'), ('ABMUsuarios'), ('ABMClientes'), ('ABMHoteles'),
+('ABMHabitaciones'), ('ABMRegímenesEstadía'), ('ABMEstadías'), ('ModificarUsuarios');
+
