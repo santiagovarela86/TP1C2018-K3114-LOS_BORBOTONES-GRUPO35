3e4fbfe3a5b66e42c725dd447c2eea29d43dfbc2
santiagovarela86 <santiagovarela.86@gmail.com>
2018-05-30 00:34:10 -0300

    Se genera el modelo del Consumible. Se corrige en el DER la relacion entre Consumible e ItemFactura

---
+++ Modelo/Consumible.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrbaHotel.Modelo
+{
+    public class Consumible
+    {
+        private int idConsumible = 0;
+        private int codigo = 0;
+        private String descripcion = "";
+        private float precio = 0;
+
+        public Consumible(int idConsumible, int codigo, String descripcion, float precio)
+        {
+            this.idConsumible = idConsumible;
+            this.codigo = codigo;
+            this.descripcion = descripcion;
+            this.precio = precio;
+        }
+
+        public int getIdConsumible()
+        {
+            return this.idConsumible;
+        }
+
+        public int getCodigo()
+        {
+            return this.codigo;
+        }
+
+        public String getDescripcion()
+        {
+            return this.descripcion;
+        }
+
+        public float getPrecio()
+        {
+            return this.precio;
+        }
+
+        //Estos metodos extra los necesito para popular los combo box y data grid view
+        public int IdConsumible { get { return this.getIdConsumible(); } }
+        public int Codigo { get { return this.getCodigo(); } }
+        public String Descripcion { get { return this.getDescripcion(); } }
+        public float Precio { get { return this.getPrecio(); } }
+        
+    }
+}
