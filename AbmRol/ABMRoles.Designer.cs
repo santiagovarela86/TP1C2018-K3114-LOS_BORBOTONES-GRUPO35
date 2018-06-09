6ccdf6d29dcb86d96d054c5f989db9fbd4454f1b
santiagovarela86 <santiagovarela.86@gmail.com>
2018-06-09 16:12:42 -0300

    Se saca la busqueda dinamica en ABM Usuario, detalles de la UI

--- AbmRol/ABMRoles.Designer.cs
+++ AbmRol/ABMRoles.Designer.cs
@@ -71,7 +71,7 @@
             this.buscar.Location = new System.Drawing.Point(479, 61);
             this.buscar.Name = "buscar";
             this.buscar.Size = new System.Drawing.Size(75, 23);
-            this.buscar.TabIndex = 7;
+            this.buscar.TabIndex = 4;
             this.buscar.Text = "Buscar";
             this.buscar.UseVisualStyleBackColor = true;
             this.buscar.Click += new System.EventHandler(this.buscar_Click);
@@ -81,7 +81,7 @@
             this.button1.Location = new System.Drawing.Point(398, 61);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 6;
+            this.button1.TabIndex = 3;
             this.button1.Text = "Limpiar";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -93,7 +93,7 @@
             this.comboBoxFuncionalidades.Location = new System.Drawing.Point(419, 25);
             this.comboBoxFuncionalidades.Name = "comboBoxFuncionalidades";
             this.comboBoxFuncionalidades.Size = new System.Drawing.Size(135, 21);
-            this.comboBoxFuncionalidades.TabIndex = 5;
+            this.comboBoxFuncionalidades.TabIndex = 2;
             // 
             // label3
             // 
@@ -112,7 +112,7 @@
             this.comboBoxEstados.Location = new System.Drawing.Point(227, 26);
             this.comboBoxEstados.Name = "comboBoxEstados";
             this.comboBoxEstados.Size = new System.Drawing.Size(107, 21);
-            this.comboBoxEstados.TabIndex = 3;
+            this.comboBoxEstados.TabIndex = 1;
             // 
             // label2
             // 
@@ -166,7 +166,7 @@
             this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridView1.Size = new System.Drawing.Size(547, 150);
-            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.TabIndex = 5;
             this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             // 
             // groupBox3
@@ -187,7 +187,7 @@
             this.button5.Location = new System.Drawing.Point(173, 20);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(75, 23);
-            this.button5.TabIndex = 2;
+            this.button5.TabIndex = 8;
             this.button5.Text = "Baja";
             this.button5.UseVisualStyleBackColor = true;
             // 
@@ -197,7 +197,7 @@
             this.button4.Location = new System.Drawing.Point(91, 20);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 1;
+            this.button4.TabIndex = 7;
             this.button4.Text = "Modificación";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
@@ -207,7 +207,7 @@
             this.button3.Location = new System.Drawing.Point(9, 20);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 0;
+            this.button3.TabIndex = 6;
             this.button3.Text = "Alta";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
